8eefc6f35e2f8b4290ab5ef723c769641cdcbb8a
toanvipboy12345 <tminh3301747@gmail.com>
2025-07-02 16:30:44 +0700

    new routes

--- music-app/routes/userRoutes.js
+++ music-app/routes/userRoutes.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const playlistController = require('../controllers/playlistController');
-// const isUser = require('../middleware/User');
-
-// // Playlist routes for authenticated users
-// // Đặt route tĩnh trước để ưu tiên khớp
-// router.get('/playlists/user/:userId/summary', isUser, playlistController.getUserPlaylistsSummary);
-// router.get('/playlists/user/:userId/:playlistId', isUser, playlistController.getPlaylistByUserId);
-// router.post('/playlists', isUser, playlistController.createPlaylist);
-// router.post('/playlists/:playlistId/songs', isUser, playlistController.addSongToPlaylist);
-// router.delete('/playlists/:playlistId', isUser, playlistController.deletePlaylist);
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const playlistController = require('../controllers/playlistController');
@@ -26,7 +12,6 @@ router.post('/playlists/:playlistId/songs', isUser, playlistController.addSongTo
 router.delete('/playlists/:playlistId', isUser, playlistController.deletePlaylist);
 
 // Queue routes for authenticated users
-// Queue routes for authenticated users
 router.get('/queue', isUser, queueController.getUserQueue);
 router.post('/queue/add', isUser, queueController.addSongToQueue);
 router.delete('/queue/remove/:song_id', isUser, queueController.removeSongFromQueue);
@@ -34,4 +19,6 @@ router.put('/queue/update-current', isUser, queueController.updateCurrentSong);
 router.post('/queue/next', isUser, queueController.nextSong);
 router.post('/queue/prev', isUser, queueController.prevSong);
 router.delete('/queue/clear', isUser, queueController.clearQueue);
+router.post('/queue/play-content', isUser, queueController.playContent); // Thêm endpoint mới
+
 module.exports = router;
