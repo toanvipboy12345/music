6b04c59a8b7bb4345c1afb1be55dbd458c3e764e
toanvipboy12345 <tminh3301747@gmail.com>
2025-07-11 00:51:53 +0700

    no changes made

--- client--app/src/App.tsx
+++ client--app/src/App.tsx
@@ -10,9 +10,12 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 import { MainLayout } from './components/layout/MainLayout';
 import { CollectionDetail } from './pages/CollectionDetail';
 import { PlaylistDetail } from './pages/PlaylistDetail';
-import  {AlbumDetail}  from './pages/AlbumDetail'; // Import AlbumDetail
+import { AlbumDetail } from './pages/AlbumDetail';
 import SearchPage from './pages/SearchPage';
-import {ArtistDetail} from './pages/ArtistDetail';
+import { ArtistDetail } from './pages/ArtistDetail';
+import PremiumPlans from './pages/PremiumPlans';
+import PremiumSubscribe from './pages/PremiumSubscribe';
+import PaymentResult from './pages/PaymentResult';
 
 const App: React.FC = () => {
   return (
@@ -23,12 +26,17 @@ const App: React.FC = () => {
             <Route path="/" element={<MainLayout />}>
               <Route index element={<Home />} />
               <Route path="/search/:query" element={<SearchPage />} />
+              <Route path="/premium" element={<PremiumPlans />} />
+              <Route path="/premium/subscribe/:plan_id" element={<PremiumSubscribe />} />
+              <Route path="/payment-result" element={<PaymentResult />} />
+              <Route path="/payment/success" element={<PaymentResult />} />
+              <Route path="/payment/failure" element={<PaymentResult />} />
               <Route path="/search/:query/artists" element={<SearchPage />} />
               <Route path="/search/:query/albums" element={<SearchPage />} />
               <Route path="/search/:query/tracks" element={<SearchPage />} />
               <Route path="/collection/:artist_id" element={<CollectionDetail />} />
               <Route path="/artists/:artist_id" element={<ArtistDetail />} />
-              <Route path="/albums/:album_id" element={<AlbumDetail />} /> {/* Thêm route cho AlbumDetail */}
+              <Route path="/albums/:album_id" element={<AlbumDetail />} />
               <Route
                 path="/playlists/:playlistId"
                 element={
